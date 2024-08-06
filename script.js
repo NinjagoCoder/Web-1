2a99d89f74bb6c98ecdaa142b4b1e602bfea5bb4
The Syob <michnalex3@gmail.com>
2024-08-06 00:44:12 -0400

    Upd

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const button = document.getElementById("Click")
-
+let i;
 function handleclick(){
     alert("Best random website ever")
+    console.log(i++)
+    
 }
 
 button.addEventListener('click',handleclick)
+
+
+
